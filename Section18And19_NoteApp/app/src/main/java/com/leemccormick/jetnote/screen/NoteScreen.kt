fab666045130b0fb6a2f2b9d89e5e126fab4433d
Lee McCormick <lee.mccormick@dialexa.com>
2023-01-11 16:56:40 -0600

    Working on Section18 Note App to continue on Lecture122

---
+++ Section18And19_NoteApp/app/src/main/java/com/leemccormick/jetnote/screen/NoteScreen.kt
@@ -0,0 +1,72 @@
+package com.leemccormick.jetnote.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.leemccormick.jetnote.R
+import com.leemccormick.jetnote.components.NoteButton
+import com.leemccormick.jetnote.components.NoteInputText
+
+@Composable
+fun NoteScreen() {
+    var title by remember {
+        mutableStateOf("")
+    }
+
+    var description by remember {
+        mutableStateOf("")
+    }
+
+    Column(modifier = Modifier.padding(6.dp)) {
+        TopAppBar(title = {
+            Text(text = stringResource(id = R.string.app_name))
+        }, actions = {
+            Icon(imageVector = Icons.Rounded.Notifications, contentDescription = "Icon")
+        }, backgroundColor = Color(0xFFDADFE3))
+
+        // Content Here...
+        Column(modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+            NoteInputText(
+                modifier = Modifier.padding(top = 9.dp, bottom = 8.dp),
+                text = title,
+                label = "Title" ,
+                onTextChange = {
+                    if (it.all { char -> char.isLetter() || char.isWhitespace() })
+                        title = it
+                }
+            )
+
+            NoteInputText(
+                modifier = Modifier.padding(top = 9.dp, bottom = 8.dp),
+                text = description,
+                label = "Add a note" ,
+                onTextChange = {
+                    if (it.all { char -> char.isLetter() || char.isWhitespace() })
+                        description = it
+                }
+            )
+            
+            NoteButton(text = "Save", onClick = { /*TODO*/ })
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NoteScreenPreview() {
+    NoteScreen()
+}
