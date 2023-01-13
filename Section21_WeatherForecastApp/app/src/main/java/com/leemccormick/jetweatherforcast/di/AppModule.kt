a145a8dabd458648954b543a513d5b158cb1ab2b
Lee McCormick <lee.mccormick@dialexa.com>
2023-01-13 14:57:00 -0600

    Started Weather App, to continue on lecture 172

---
+++ Section21_WeatherForecastApp/app/src/main/java/com/leemccormick/jetweatherforcast/di/AppModule.kt
@@ -0,0 +1,3 @@
+package com.leemccormick.jetweatherforcast.di
+
+class AppModule {}
---
+++ Section21_WeatherForecastApp/app/src/main/java/com/leemccormick/jetweatherforcast/navigation/WeatherScreens.kt
@@ -0,0 +1,10 @@
+package com.leemccormick.jetweatherforcast.navigation
+
+enum class WeatherScreens {
+    SplashScreen,
+    MainScreen,
+    AboutScreen,
+    FavoriteScreen,
+    SearchScreen,
+    SettingsScreen
+}
