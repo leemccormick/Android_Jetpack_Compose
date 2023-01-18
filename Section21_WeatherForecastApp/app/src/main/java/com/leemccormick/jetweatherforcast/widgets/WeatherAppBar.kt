57d7ee2c8e5032da96ff8691504e45eb4f6cc3b2
Lee McCormick <lee.mccormick@dialexa.com>
2023-01-18 16:59:39 -0600

    Added Toast when favorite city.

--- Section21_WeatherForecastApp/app/src/main/java/com/leemccormick/jetweatherforcast/widgets/WeatherAppBar.kt
+++ Section21_WeatherForecastApp/app/src/main/java/com/leemccormick/jetweatherforcast/widgets/WeatherAppBar.kt
@@ -1,5 +1,7 @@
 package com.leemccormick.jetweatherforcast.widgets
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -13,6 +15,7 @@ import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
@@ -23,6 +26,7 @@ import androidx.navigation.NavController
 import com.leemccormick.jetweatherforcast.model.Favorite
 import com.leemccormick.jetweatherforcast.navigation.WeatherScreens
 import com.leemccormick.jetweatherforcast.screens.favorites.FavoriteViewModel
+import kotlinx.coroutines.flow.collect
 
 @Composable
 fun WeatherAppBar(
@@ -38,6 +42,12 @@ fun WeatherAppBar(
     val showDialog = remember {
         mutableStateOf(false)
     }
+
+    val showIt = remember {
+        mutableStateOf(false)
+    }
+
+    val context = LocalContext.current
 
     if (showDialog.value) {
         ShowSettingDropDownMenu(showDialog = showDialog, navController = navController)
@@ -78,28 +88,49 @@ fun WeatherAppBar(
             }
 
             if (isMainScreen) {
-                Icon(
-                    imageVector = Icons.Default.Favorite,
-                    contentDescription = "favorite icon",
-                    modifier = Modifier
-                        .scale(0.9f)
-                        .clickable {
-                            val cityAndCountry = title.split(",")
+                val isAlreadyFavList =
+                    favoriteViewModel.favList.collectAsState().value.filter { item ->
+                        (item.city == title.split(",")[0])
+                    }
 
-                            favoriteViewModel.insertFavorite(
-                                Favorite(
-                                    city = cityAndCountry[0],
-                                    country = cityAndCountry[1]
-                                )
-                            )
-                        },
-                    tint = Color.Red.copy(alpha = 0.6f)
-                )
+                if (isAlreadyFavList.isNullOrEmpty()) {
+                    Icon(
+                        imageVector = Icons.Default.Favorite,
+                        contentDescription = "favorite icon",
+                        modifier = Modifier
+                            .scale(0.9f)
+                            .clickable {
+                                val cityAndCountry = title.split(",")
+
+                                favoriteViewModel.insertFavorite(
+                                    Favorite(
+                                        city = cityAndCountry[0],
+                                        country = cityAndCountry[1]
+                                    )
+                                ).run {  // After done inserting favorite, then run this...
+                                    showIt.value = true
+                                }
+                            },
+                        tint = Color.Red.copy(alpha = 0.6f)
+                    )
+                }
+            } else {
+                showIt.value = false
+                Box {}
             }
+
+            ShowToast(context = context, showIt)
         },
         backgroundColor = Color.Transparent,
         elevation = elevation
     )
+}
+
+@Composable
+fun ShowToast(context: Context, showIt: MutableState<Boolean>) {
+    if (showIt.value) {
+        Toast.makeText(context, "City Added to Favorites.", Toast.LENGTH_SHORT).show()
+    }
 }
 
 @Composable
